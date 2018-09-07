155b4f2ed1ba11b72c495ec69380682e990245bd
Tom Jordan <mail@tom-jordan.co.uk>
2018-09-07 15:08:35 +0100

    Production build

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function HTMLTemplate(reactDom) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico" />
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="/main.css">
 </head>
 <body>
 <div id="app">${reactDom}</div>
